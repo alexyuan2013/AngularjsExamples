e9535abb11f35a6ddb2341bd3252c52a28b664f4
Alex <alexyuan1988@gmail.com>
2016-05-23 23:00:16 +0800

    注释，$watch， promise

--- chapter02/app/js/7MinWorkout/workout.js
+++ chapter02/app/js/7MinWorkout/workout.js
@@ -1,6 +1,8 @@
 angular.module('7minWorkout').controller('WorkoutController', 
     ['$scope','$interval', function($scope, $interval){
-    
+    /**
+     * 练习类
+     */
     function Exercise(args){
         this.name = args.name;
         this.title = args.title;
@@ -11,13 +13,18 @@ angular.module('7minWorkout').controller('WorkoutController',
         this.nameSound = args.nameSound;
         this.procedure = args.procedure;
     }
-
+    /**
+     * 训练类，包含多个练习
+     */
     function WorkoutPlan(args){
         this.exercises = [];
         this.name = args.name;
         this.title = args.title;
         this.restBetweenExercise = args.restBetweenExercise;
     }
+    /**
+     * 创建一个训练计划，类似与工厂方法
+     */
     var createWorkout = function(){
         var workout = new WorkoutPlan({
             name: "7minWorkout",
@@ -36,6 +43,9 @@ angular.module('7minWorkout').controller('WorkoutController',
         });
         return workout;
     }
+    /**
+     * 开始练习
+     */
     var startExercise = function (exercisePlan){
         $scope.currentExercise = exercisePlan;
         $scope.currentExerciseDuration = 0;
@@ -43,14 +53,24 @@ angular.module('7minWorkout').controller('WorkoutController',
             ++$scope.currentExerciseDuration;
         },
         1000,
-        $scope.currentExercise.duration);
+        $scope.currentExercise.duration)
+        .then(function(){//采用promise借口来实现同步
+            var next = getNextExercise(exercisePlan);
+            if(next){
+                startExercise(next);
+            } else {
+                console.log("Workout complete");
+            }
+        });
     }
     
     
     var restExercise;
     var workoutPlan;
    
-    
+    /**
+     * 启动训练
+     */
     var startWorkout = function(){
         workoutPlan = createWorkout();
         restExercise = {
@@ -64,7 +84,36 @@ angular.module('7minWorkout').controller('WorkoutController',
         };
         startExercise(workoutPlan.exercises.shift());
     }
-    
+    /**
+     * 进行下一个练习
+     */
+    var getNextExercise = function(currentExercisePlan){
+        var nextExercise = null;
+        if(currentExercisePlan === restExercise){
+            nextExercise = workoutPlan.exercise.shift();
+        } else {
+            if(workoutPlan.exercises.length != 0){
+                nextExercise = restExercise;
+            }
+        }
+    };
+    /**
+     * 监视练习是否完成</br>
+     * 未使用
+     */
+    // $scope.$watch('currentExerciseDuration', function(nVal){
+    //    if(nVal == $scope.currentExercise.duration){
+    //        var next = getNextExercise($scope.currentExercise);
+    //        if(next) {
+    //            startExercise(next);
+    //        } else {
+    //            console.log("Workout complete!");
+    //        }
+    //    } 
+    // });
+    /**
+     * 初始化方法
+     */
     var init = function(){
         startWorkout();
     }
