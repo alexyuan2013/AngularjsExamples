073c746338a7d6a4551b05f5fb3de3840cd857b6
Alex <alexyuan1988@gmail.com>
2016-06-06 23:56:48 +0800

    chapter 3 first commit

---
+++ chapter03/app/js/app.js
@@ -0,0 +1,26 @@
+angular.module('app', ['ngRoute','ngSanitize','7minWorkout'])
+.config(function ($routeProvider, $sceDelegateProvider){//配置路由表
+    $routeProvider.when('/start', {
+        templateUrl: 'partials/start.html'
+    });
+    $routeProvider.when('/workout', {
+        templateUrl: 'partials/workout.html',
+        controller: 'WorkoutController'
+    });
+    $routeProvider.when('/finish', {
+        templateUrl: 'partials/finish.html'
+    });
+    $routeProvider.otherwise({
+        redirectTo: '/start'
+    });
+    
+    $sceDelegateProvider.resourceUrlWhitelist([
+       'self',
+       'http://*.youtube.com/**' 
+    ]);
+});
+
+angular.module('7minWorkout', []);
+
+
+
